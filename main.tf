0d077f8d0cc59ba4e27f75145e8dc8848535401b
Fabio Sousa Vieira <fabio.sousavieira@vcontractor.co.za>
2023-03-20 15:35:13 +0200

    Finalised TF with GH Actions template

--- main.tf
+++ main.tf
@@ -16,27 +16,3 @@ terraform {
 provider "aws" {
   region = "af-south-1"
 }
-
-# resource "aws_iam_role" "test_role" {
-#   name = "NewTestRole"
-
-#   # Terraform's "jsonencode" function converts a
-#   # Terraform expression result to valid JSON syntax.x	
-#   assume_role_policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Action = "sts:AssumeRole"
-#         Effect = "Allow"
-#         Sid    = ""
-#         Principal = {
-#           Service = "ec2.amazonaws.com"
-#         }
-#       },
-#     ]
-#   })
-
-#   tags = {
-#     tag-key = "tag-value"
-#   }
-# }
